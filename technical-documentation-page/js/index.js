960e1c2d2c6c22ec0781f2e6a18e453cbbee0c14
ronb <ronbnj@gmail.com>
2018-07-09 19:22:05 -0400

    fixed scrolling issue on page

--- technical-documentation-page/js/index.js
+++ technical-documentation-page/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     /* Animation on scroll */
     $(function() {
       $("a[href*='#']:not([href='#'])").click(function() {
-         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         if (location.pathname.replace(/[?'/,]/g,'') == this.pathname.replace(/[?'/,]/g,'') && location.hostname == this.hostname) {
               var target = $(this.hash);
               target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
               if (target.length) {
